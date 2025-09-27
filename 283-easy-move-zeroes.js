25925266e6c6d52d25736794dd709a3fa12e7148
Rishita Rallabandi <rallabandirishita@gmail.com>
2025-09-27 13:58:51 +0100

    Create 283-easy-move-zeroes.js

---
+++ 283-easy-move-zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let l = 0;
+
+    for (let r = 0; r < nums.length; r++) {
+        if (nums[r] !== 0) {
+            temp = nums[r];
+            nums[r] = nums[l];
+            nums[l] = temp;
+            l++
+        }
+    }
+};
